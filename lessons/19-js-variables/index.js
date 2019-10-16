9550f5ca091eabee2d9670f617b8c061af86917c
namtranhoai <namtranhoai@gmail.com>
2019-10-15 19:45:26 -0600

    Completed lesson 19

---
+++ lessons/19-js-variables/index.js
@@ -0,0 +1,27 @@
+const MONTH_OF_BIRTH ="November";
+const FIRST_NAME = "Nam";
+const LAST_NAME = "Tran"
+
+/*
+    let monthOfBirth;
+    let firstName;
+    let lastName;
+*/
+let city;
+let age;
+let nextCity;
+
+city = "Saskatoon";
+age = "38";
+nextCity = city;
+
+console.log("Month Of Birth:", MONTH_OF_BIRTH);
+console.log("First Name:", FIRST_NAME);
+console.log("Last Name:", LAST_NAME);
+console.log("City:", city);
+console.log("Age:", age);
+console.log("Next City:", nextCity);
+console.log("Full Name:", `${FIRST_NAME} ${LAST_NAME}`);
+
+
+
