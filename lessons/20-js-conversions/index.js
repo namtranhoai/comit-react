5065a1265a6f6137a2c099dab5e5152b073d2064
namtranhoai <namtranhoai@gmail.com>
2019-10-15 22:39:14 -0600

    Completed lesson

---
+++ lessons/20-js-conversions/index.js
@@ -0,0 +1,73 @@
+let age = 38;
+let firstName = "Nam";
+let courseState = Boolean(false); //state whether completed this course yet
+let finalScore = null;
+
+let winningNumbers;
+
+
+console.log("age:", age);
+console.log("First name:", firstName);
+console.log("Course state:", courseState);
+console.log("Final score:", finalScore);
+console.log("Winning numbers:", winningNumbers);
+
+console.log("----------------------------------------");
+console.log("Type of age:", typeof age);
+console.log("---- Number to String ------------------");
+console.log("New type:", typeof String(age));
+console.log("Age to string", String(age));
+
+console.log("---- Number to Boolean ------------------");
+console.log("New type:", typeof Boolean(age));
+console.log("Age to string", Boolean(age));
+
+console.log("----------------------------------------");
+console.log("Type of firstName:", typeof firstName);
+console.log("---- String to Number ------------------");
+console.log("New type:", typeof Number(firstName));
+console.log("First Name to Number", Number(firstName));
+
+console.log("---- String to Boolean ------------------");
+console.log("New type:", typeof Boolean(firstName));
+console.log("First Name to Boolean", Boolean(firstName));
+
+console.log("----------------------------------------");
+console.log("Type of Course state:", typeof courseState);
+console.log("---- Boolean to String ------------------");
+console.log("New type:", typeof String(courseState));
+console.log("New value", String(courseState));
+
+console.log("---- Boolean to Number ------------------");
+console.log("New type:", typeof Number(courseState));
+console.log("New value", Number(courseState));
+
+console.log("----------------------------------------");
+console.log("Type of Final score:", typeof finalScore);
+console.log("---- to String ------------------");
+console.log("New type:", typeof String(finalScore));
+console.log("New value", String(finalScore));
+
+console.log("---- to Number ------------------");
+console.log("New type:", typeof Number(finalScore));
+console.log("New value", Number(finalScore));
+
+console.log("---- to Boolean ------------------");
+console.log("New type:", typeof Boolean(finalScore));
+console.log("New value", Boolean(finalScore));
+
+console.log("----------------------------------------");
+console.log("Type of winning numbers:", typeof winningNumbers);
+console.log("---- to String ------------------");
+console.log("New type:", typeof String(winningNumbers));
+console.log("New value", String(winningNumbers));
+
+console.log("---- to Number ------------------");
+console.log("New type:", typeof Number(winningNumbers));
+console.log("New value", Number(winningNumbers));
+
+console.log("---- to Boolean ------------------");
+console.log("New type:", typeof Boolean(winningNumbers));
+console.log("New value", Boolean(winningNumbers));
+
+
